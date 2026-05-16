0c2cca9f03f5689a131abdb52cf9a1fc047fcfcd
SravaniBeesetti <beesettisravani@gmail.com>
2026-05-16 13:00:09 +0530

    Updated production mongodb uri

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+  },
+  {
+    collection: "users",
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
