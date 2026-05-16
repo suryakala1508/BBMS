34a99c1820ea0ec634456bf312289ced49a1333f
SravaniBeesetti <beesettisravani@gmail.com>
2026-05-16 13:11:02 +0530

    fixed mongo uri typo

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error(err.message);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("MongoDB Error:", error.message);
     process.exit(1);
   }
 };
