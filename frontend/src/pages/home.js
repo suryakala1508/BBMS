74443d0b88f34d5fa4d940861013bd946bf644b7
B.suryakala <beesettisuryakala@gmail.com>
2026-06-10 17:43:19 +0530

    Update home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -18,7 +18,7 @@ function Home() {
     if (!query) return alert('Please enter a blood type');
 
     try {
-      const res = await fetch(`https://bbms-production-62ee.up.railway.app/api/donors/search?q=${encodeURIComponent(query)}`);
+      const res = await fetch(`https://bbms-4j6u.onrender.com/api/donors/search?q=${encodeURIComponent(query)}`);
       const data = await res.json();
 
       if (res.ok && Array.isArray(data) && data.length > 0) {
